8c65d262c8ebf682b83579f7083f33b6021d3948
clouddrove-ci <anmol+ci@clouddrove.com>
2026-05-09 14:23:57 +0000

    fix(outputs): scope output description to nfs_host

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "nfs_id" { value = try(digitalocean_nfs.this[0].id, null) }
-output "nfs_host" { value = try(digitalocean_nfs.this[0].host, null) }
-output "nfs_mount_path" {
-  description = "Value exported by output `nfs_mount_path`."
-  value       = try(digitalocean_nfs.this[0].mount_path, null)
+output "nfs_host" {
+  description = "Value exported by output `nfs_host`."
+  value       = try(digitalocean_nfs.this[0].host, null)
 }
+output "nfs_mount_path" { value = try(digitalocean_nfs.this[0].mount_path, null) }
 output "nfs_status" { value = try(digitalocean_nfs.this[0].status, null) }
 output "nfs_performance_tier" { value = try(digitalocean_nfs.this[0].performance_tier, null) }
 output "snapshot_id" { value = try(digitalocean_nfs_snapshot.this[0].id, null) }
