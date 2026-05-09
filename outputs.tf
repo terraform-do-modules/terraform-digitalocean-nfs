d1156417fb27848bc2690876088696c9dd8717e2
clouddrove-ci <anmol+ci@clouddrove.com>
2026-05-09 18:12:50 +0000

    fix: add description for nfs_id output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
-output "nfs_id" { value = try(digitalocean_nfs.this[0].id, null) }
+output "nfs_id" {
+  description = "Output value for nfs_id."
+  value       = try(digitalocean_nfs.this[0].id, null)
+}
 output "nfs_host" { value = try(digitalocean_nfs.this[0].host, null) }
 output "nfs_mount_path" { value = try(digitalocean_nfs.this[0].mount_path, null) }
 output "nfs_status" { value = try(digitalocean_nfs.this[0].status, null) }
